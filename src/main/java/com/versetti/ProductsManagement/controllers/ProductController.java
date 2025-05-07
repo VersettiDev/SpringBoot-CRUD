4d2bb80f47938ee39d97d9d06b0916b642c5155b
Versetti <brunomuryllo.nt@gmail.com>
2025-05-07 15:33:38 -0300

    feat: create method saveNewProduct (POST)

--- src/main/java/com/versetti/ProductsManagement/controllers/ProductController.java
+++ src/main/java/com/versetti/ProductsManagement/controllers/ProductController.java
@@ -1,13 +1,13 @@
 package com.versetti.ProductsManagement.controllers;
 
+import com.versetti.ProductsManagement.DTOs.ProductDto;
 import com.versetti.ProductsManagement.entity.Product;
 import com.versetti.ProductsManagement.repositories.ProductRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +22,13 @@ public class ProductController {
     public ResponseEntity getAllProducts () {
         List<Product> listProducts = repository.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(listProducts);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewProduct (@RequestBody ProductDto dto) {
+        var newProduct = new Product();
+        BeanUtils.copyProperties(dto, newProduct);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(newProduct));
     }
 }
