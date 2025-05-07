45495e94acb80648e231d6042427083063acc8d7
Versetti <brunomuryllo.nt@gmail.com>
2025-05-07 15:28:05 -0300

    feat: create method getAllProducts (GET)

--- src/main/java/com/versetti/ProductsManagement/controllers/ProductController.java
+++ src/main/java/com/versetti/ProductsManagement/controllers/ProductController.java
@@ -1,10 +1,26 @@
 package com.versetti.ProductsManagement.controllers;
 
+import com.versetti.ProductsManagement.entity.Product;
+import com.versetti.ProductsManagement.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("v1/products")
 public class ProductController {
 
+    @Autowired
+    ProductRepository repository;
+
+    @GetMapping
+    public ResponseEntity getAllProducts () {
+        List<Product> listProducts = repository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(listProducts);
+    }
 }
